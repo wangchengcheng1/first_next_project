f3486777dbdcf11dcc0315156a81c55e1cd05ae8
pw.dev15 <wanghongcheng10@gmail.com>
2024-11-15 18:25:53 +0900

    默认不访问id时候的访问的网页

---
+++ src/pages/post/index.tsx
@@ -0,0 +1,11 @@
+// pages/404.js
+// import Link from 'next/link';
+// import { useRouter } from 'next/router';
+export default function Custom404() {
+    // const router=useRouter();
+    return (
+      <div style={{ textAlign: 'center', padding: '50px' }}>
+       iam post
+      </div>
+    );
+  }
